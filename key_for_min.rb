24448df9244d1e4f0247bde1e4e5bf981d51c855
Adam Dudley <dudleyadam@gmail.com>
2018-11-30 19:41:01 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,15 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  name_hash.map do |key, value| #this iterates over the hash
+  key.sort.to_h do |key1, key2| #this returns the key
+    key1 <=> key2
+  # key.each do |key1, key2|
+  #   if key1 < key2
+  #     key1
+  #   end
+  #now I need to compare the value of the elements in the set of keys
+  #now I need to return the key in the set with the smallest value
+    end
+  end
 end
